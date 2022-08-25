7ede9231bd30c3f81ccd46962d0fbaa2920ea934
David-isaiah Ngouambeu <david-isaiah.ngouambeu@my.tccd.edu>
2022-08-24 21:36:23 -0500

    Creation of Hello world and Section 1

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿// David-Isaiah
+//ITSE 1430 2022 Fall
+//Lab #
+namespace HelloWorld
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
