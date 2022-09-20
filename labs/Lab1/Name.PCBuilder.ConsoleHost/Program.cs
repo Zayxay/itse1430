1404bed61cf6aa76590b866550ea2c3164d493e3
david-isaiah <Isaiahngouambeu@gmail.com>
2022-09-19 19:09:49 -0500

    start

---
+++ labs/Lab1/Name.PCBuilder.ConsoleHost/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("David-Isaiah Ngouambeu");
+Console.WriteLine("ITSE 1430");
+Console.WriteLine("09/15/2022");
+
+// ConsoleKeyInfo -here we have the keyword for using console key. it tells the compiler what type of value the identifier will hold
+// ConsoleKeyInfo key = Console.ReadKey() - The console read key function tells the compiler we are looking for the user to return us a key
+//Use readkey to read sensitive data like passwords. 
+
+
+
+
+
+
+//Functions
+
