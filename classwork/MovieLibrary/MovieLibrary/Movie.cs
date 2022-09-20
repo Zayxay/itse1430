09460ca031dc42ddfa0a10ae157d731707cc66db
David-isaiah Ngouambeu <david-isaiah.ngouambeu@my.tccd.edu>
2022-09-19 21:39:40 -0500

    unit 2

---
+++ classwork/MovieLibrary/MovieLibrary/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieLibrary
+{
+     //Represents a movie
+     /// <summary>
+     /// Represents a movie
+     /// </summary>
+    public class Movie
+    {
+      public  string title = "";
+      public  string description = "";
+      public  int runLength = 0; //In minutes
+      public  int releaseYear = 1900;
+      public  string rating = "";
+      public  bool isClassic = false;
+    }
+}
--- labs/Lab1/Name.PCBuilder.ConsoleHost/Program.cs
+++ labs/Lab1/Name.PCBuilder.ConsoleHost/Program.cs
@@ -1,6 +1,4 @@
-﻿Console.WriteLine("David-Isaiah Ngouambeu");
-Console.WriteLine("ITSE 1430");
-Console.WriteLine("09/15/2022");
+﻿
 
 // ConsoleKeyInfo -here we have the keyword for using console key. it tells the compiler what type of value the identifier will hold
 // ConsoleKeyInfo key = Console.ReadKey() - The console read key function tells the compiler we are looking for the user to return us a key
@@ -12,4 +10,17 @@ Console.WriteLine("09/15/2022");
 
 
 //Functions
+
+ void DisplayInformation ()
+{
+    Console.WriteLine("David-Isaiah Ngouambeu");
+    Console.WriteLine("ITSE 1430");
+    Console.WriteLine("09/15/2022");
+}
+
+void DisplayMenu ()
+{
+    Console.WriteLine("Q)uit");
+}
+
 
