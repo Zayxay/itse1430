759141250c32979badd500be36aed8ac706d98e3
david-isaiah <Isaiahngouambeu@gmail.com>
2022-09-24 02:33:17 -0500

    Story 7 done

--- labs/Lab1/Name.PCBuilder.ConsoleHost/CodeFile3.cs
+++ labs/Lab1/Name.PCBuilder.ConsoleHost/CodeFile3.cs
@@ -47,7 +47,8 @@ class Shop
         Console.WriteLine("Option 1. New Order");
         Console.WriteLine("Option 2. View Order");
         Console.WriteLine("Option 3. Clear Order");
-        Console.WriteLine("Option 4. Quit");
+        Console.WriteLine("Option 4. Modify Oredr");
+        Console.WriteLine("Option 5. Quit");
 
 
 
@@ -63,7 +64,8 @@ class Shop
                 case 1: Run(); break;
                 case 2: ViewOrder(_processorName, _processorPrice, _memName, _memPrice, _orderTotal); break;
                 case 3: ClearOrder(_processorName, _processorPrice, _memName, _memPrice, _orderTotal); break;
-                case 4: DisplayOutro(); break;
+                case 4: ModifyOrder(_processorName, _processorPrice, _memName, _memPrice, _orderTotal); break;
+                case 5: DisplayOutro(); break;
                 default: Error(); break;
             }
         } catch (FormatException)
@@ -204,20 +206,87 @@ class Shop
         {
             Console.WriteLine($"There is no order yet.        Cart:{cart:C2}");
             DisplayMenu();
-        }
-        else
+        } else
         {
             Console.WriteLine("Here are your current componets");
             Console.WriteLine("========================");
-            Console.WriteLine($"{itemName} for {icost}");
-            Console.WriteLine($"{memoryName} for {memoryCost}");
-            return;
+            Console.WriteLine($"{itemName:C2} for {icost:C2}");
+            Console.WriteLine($"{memoryName:C2} for {memoryCost:C2}");
+            Console.WriteLine("==============");
+            Console.WriteLine("Press 1)  If you would like to modify your processor");
+            Console.WriteLine("Press 2)  If you would like to modify your memory");
+            Console.WriteLine("Press 7) To return back to the menu");
+            int response = Int32.Parse(Console.ReadLine());
+            switch (response)
+            {
+                case 1: ModifyProcessor(); break;
+                case 2: ModifyMemory(); break;
+                case 7:  DisplayMenu(); break;
+
+            }
+
+
         }
 
 
     }
 
+    private void ModifyProcessor (  )
+    {
+        Console.Clear();
+        Console.WriteLine("Press 1----- To buy the AMD Ryzen 9 5900X Processor for $1410.00");
+        Console.WriteLine("Press 2----- To buy the AMD Ryzen 7 5700X Processor for $1270.00");
+        Console.WriteLine("Press 3----- To buy the AMD Ryzen 5 5600X Processor for $1200.00");
+        Console.WriteLine("Press 4----- To buy the Intel i9-12900K Processor for $1590.00");
+        Console.WriteLine("Press 5----- To buy the Intel i7-12700K Processor for $1400.00");
+        Console.WriteLine("Press 6----- To buy the Intel i5-12600K Processor for $1280.00");
+        int response = Int32.Parse(Console.ReadLine());
+        try
+        {
+            switch (response)
+            {
+                case 1: _processorName="AMD Ryzen 9 5900X"; _processorPrice=1410; _orderTotal=_processorPrice + _memPrice; DisplayMenu(); break;
+                case 2: _processorName="AMD Ryzen 7 5700X"; _processorPrice=1270; _orderTotal=_processorPrice + _memPrice; DisplayMenu(); break;
+                case 3: _processorName="AMD Ryzen 5 5600X"; _processorPrice=1200; _orderTotal=_processorPrice + _memPrice; DisplayMenu(); break;
+                case 4: _processorName="Intel i9-12900K"; _processorPrice=1590; _orderTotal=_processorPrice + _memPrice; DisplayMenu(); break;
+                case 5: _processorName="Intel i7-12700K"; _processorPrice=1400; _orderTotal=_processorPrice + _memPrice; DisplayMenu(); break;
+                case 6: _processorName="Intel i5-12600K"; _processorPrice=1200; _orderTotal=_processorPrice + _memPrice; DisplayMenu(); break;
+                default: Error(); break;
+            }
+        } catch (FormatException)
+        {
 
+            Error();
+        }
+    }
+
+    private void ModifyMemory ()
+    {
+        Console.Clear();
+        Console.WriteLine($"Your current cart total is: {_orderTotal:C2}");
+        Console.WriteLine("Press 1----- To buy 8 GB of memory for $30");
+        Console.WriteLine("Press 2----- To buy 16 GB of memory for $40");
+        Console.WriteLine("Press 3----- To buy 32 GB of memory for $90");
+        Console.WriteLine("Press 4----- To buy 64 GB for memory $410");
+        Console.WriteLine("Press 5----- To buy 128 GB of memory for $600");
+        int response = Int32.Parse(Console.ReadLine());
+        if (response <= 5)
+        {
+            switch (response)
+            {
+                case 1: _memName="8 GB"; _memPrice=30; _orderTotal= _memPrice + _processorPrice; DisplayMenu(); break;
+                case 2: _memName="16 GB"; _memPrice=40; _orderTotal=_memPrice + _processorPrice; DisplayMenu(); break;
+                case 3: _memName="32 GB"; _memPrice=90; _orderTotal=_memPrice + _processorPrice; DisplayMenu(); break;
+                case 4: _memName="64 GB"; _memPrice=410; _orderTotal = _memPrice + _processorPrice; DisplayMenu(); break;
+                case 5: _memName="128 GB"; _memPrice=600; _orderTotal=_memPrice + _processorPrice; DisplayMenu(); break;
+
+            }
+        } else
+        {
+
+            Error();
+        }
+    }
 
     private void Error ()
     {
